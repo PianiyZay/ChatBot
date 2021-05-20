844564feb49b412015a1451d8d1185775f6a7128
PianiyZay <78975213+PianiyZay@users.noreply.github.com>
2021-05-20 18:57:14 +0300

    0.001

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from random import randint
-import telebot
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-bot = telebot.TeleBot('1756397257:AAGjqG33JvOhj6AgIsrrp7KzE2cLFRyioSM')
+from config import TOKEN
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Привет!')
-    else:
-        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
-db_soup = [
-    dict(name='Борщ', рецепт='Какой-то рецепт'),
-    dict(name='Солянка', рецепт='Какой-то рецепт'),
-    dict(name='Гороховый суп', рецепт='Какой-то рецепт')
+bot = Bot(token='1756397257:AAGjqG33JvOhj6AgIsrrp7KzE2cLFRyioSM')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+def rand_recipe():
+    a = randint(0, len(db_first) - 1)
+    rand_some = str(db_first[a]['name'] + '\n' + db_first[a]['recipe'])
+    return rand_some
+
+
+db_first = [
+    dict(name='Борщ', recipe='Какой-то рецепт'),
+    dict(name='Солянка', recipe='Какой-то рецепт'),
+    dict(name='Гороховый суп', recipe='Какой-то рецепт')
 ]
 
-rand_index = randint(0, 2)
-print(db_soup[2]['name'] + '\n' + db_soup[2]['рецепт'])
 
 bot.polling(none_stop=True)
