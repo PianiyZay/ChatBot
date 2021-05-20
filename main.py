829c93125d46f347e07ade426891802a513a4457
PianiyZay <78975213+PianiyZay@users.noreply.github.com>
2021-05-20 17:55:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from random import randint
+
+db_soup = [
+    dict(name='Борщ', рецепт='Какой-то рецепт'),
+    dict(name='Солянка', рецепт='Какой-то рецепт'),
+    dict(name='Гороховый суп', рецепт='Какой-то рецепт')
+]
+
+rand_index = randint(0, 2)
+print(db_soup[2]['name'] + '\n' + db_soup[2]['рецепт'])
